d24d99bf01168904494353f347b0beef0cb9cb14
fedonch <fedonch@gmail.com>
2024-04-26 12:54:36 +0300

    Update basketLogic.js

--- js/basketLogic.js
+++ js/basketLogic.js
@@ -461,8 +461,8 @@ return new Promise((resolve, reject) => {
 document.getElementById('viewBasketAll').addEventListener('click', function() {
 var xhr = new XMLHttpRequest();
 
-var postUrl = 'https://proxyserver-z74x.onrender.com/et/pos/external-basket/v1'; // Proxy server URL
-//var postUrl = 'http://localhost:3000/et/pos/external-basket/v1'; // Proxy server URL
+// var postUrl = 'https://proxyserver-z74x.onrender.com/et/pos/external-basket/v1'; // Proxy server URL
+var postUrl = 'http://localhost:3000/et/pos/external-basket/v1'; // Proxy server URL
 //var postUrl = 'https://retail-services.cegid.cloud/t/pos/external-basket/v1'; // Proxy server URL
 xhr.open('POST', postUrl, true);
 xhr.setRequestHeader('Content-Type', 'application/json');
