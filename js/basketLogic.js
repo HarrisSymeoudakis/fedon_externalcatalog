3d0c53f3379e45d2f793024fcf4425bb7c4d882f
fedonch <fedonch@gmail.com>
2024-04-26 13:03:21 +0300

    Update basketLogic.js

--- js/basketLogic.js
+++ js/basketLogic.js
@@ -434,8 +434,8 @@ async function getToken() {
 console.log("hello");
 return new Promise((resolve, reject) => {
     var tokenRequest = new XMLHttpRequest();
-    var tokenUrl = 'https://proxyserver-z74x.onrender.com/et/as/connect/token'; // Proxy server URL 
-    //var tokenUrl = 'http://localhost:3000/et/as/connect/token'; // Proxy server URL
+    // var tokenUrl = 'https://proxyserver-z74x.onrender.com/et/as/connect/token'; // Proxy server URL 
+    var tokenUrl = 'http://localhost:3000/et/as/connect/token'; // Proxy server URL
     var tokenData = 'client_id=CegidRetailResourceFlowClient&username=AI@90478305_003_TEST&password=1234&grant_type=password&scope=RetailBackendApi offline_access'; // Construct x-www-form-urlencoded body
 
     tokenRequest.open('POST', tokenUrl, true);
@@ -509,6 +509,13 @@ var postData = {
 
 // Convert postData to JSON string
 var postDataString = JSON.stringify(postData);
+
+console.log(postDataString);
+
+// this code will run after 5 seconds
+setTimeout(function() {
+  console.log("World");
+}, 100000);
 xhr.send(postDataString);
 });
 
